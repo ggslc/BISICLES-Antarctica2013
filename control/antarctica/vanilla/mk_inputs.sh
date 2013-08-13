0bc09f812d902d71fd4e168bccf1e6b7ded6ff47
Stephen L Cornford <ggslc@bristol.ac.uk>
2013-08-13 13:15:43 +0100

    making BC inputs similar to hopper inputs

--- control/antarctica/vanilla/mk_inputs.sh
+++ control/antarctica/vanilla/mk_inputs.sh
@@ -29,9 +29,25 @@ for MAXLEV in 2 3 4; do
     for CONTROL in vanilla ; do
 	NAME=$EXPT"-"$MAXLEV"lev"
 	#PWD=`pwd`
+	SUBS="-e s/@EXPT/$EXPT/ -e s/@LEV/$MAXLEV/ -e s/@NAME/$NAME/ -e s:@PWD:$PWD: "
+	sed $SUBS inputs.$EXPT.template > scripts/inputs.$NAME	
+	sed $SUBS -e s/@NODES/$NODES/ -e s/@HOURS/$HOURS/ job.bigblue-gnu.$EXPT.template.sh > scripts/job.bigblue-gnu.$NAME.sh
+	sed $SUBS -e s/@NODES/$NODES/ -e s/@HOURS/$HOURS/ job.bigblue-intel.$EXPT.template.sh > scripts/job.bigblue-intel.$NAME.sh
+    done
+done
+
+EXPT=antarctica-petsc-ctrl
+
+for MAXLEV in 4; do
+
+    getnodes;
+
+    for CONTROL in vanilla ; do
+	NAME=$EXPT"-"$MAXLEV"lev"
+	#PWD=`pwd`
 	SUBS="-e s/@EXPT/$EXPT/ -e s/@MAXLEV/$MAXLEV/ -e s/@NAME/$NAME/ -e s:@PWD:$PWD: "
 	sed $SUBS inputs.$EXPT.template > scripts/inputs.$NAME
 	sed $SUBS -e s/@NODES/$NODES/ -e s/@HOURS/$HOURS/ job.bigblue-gnu.$EXPT.template.sh > scripts/job.bigblue-gnu.$NAME.sh
-	sed $SUBS -e s/@NODES/$NODES/ -e s/@HOURS/$HOURS/ job.bigblue-intel.$EXPT.template.sh > scripts/job.bigblue-intel.$NAME.sh
+	
     done
 done
